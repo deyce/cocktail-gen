f198f74fd951715d684c04a7b93ffc65216b4bb8
Chandon Dey <deyce@mail.uc.edu>
2024-09-06 18:16:03 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,38 @@
+//modules needed
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+//api used to generate random cocktail recipe
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { drink: "" });
+  });
+
+app.get("/generated", async (req,res) => {
+    try{
+        const result = await axios.get(API_URL);
+        const drinkData = result.data.drinks[0];
+        res.render("index.ejs", {
+            drink: drinkData.strDrink,
+            instructions: drinkData.strInstructions
+        })
+    } catch (error) {
+        console.log("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: error.message,
+        });
+      }
+    });
+
+
+//start app
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
